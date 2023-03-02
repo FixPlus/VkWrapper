205208b7ed879a498f61b855a15c7510336b3560
FixPlus <bushev.da@phystech.edu>
2023-03-03 01:00:08 +0300

    Removed swap chain retrieveImages() method
    Any implementation of swap chain will eventually need to retrive those
    images anyway. So it is better to retrive them internally upon creation
    of swapchain.

--- include/vkw/SwapChain.hpp
+++ include/vkw/SwapChain.hpp
@@ -14,6 +14,7 @@ class Fence;
 class SwapChain : public ReferenceGuard {
 public:
   SwapChain(Device &device, VkSwapchainCreateInfoKHR createInfo);
+
   SwapChain(SwapChain &&another)
       : m_device(another.m_device), m_createInfo(another.m_createInfo),
         m_swapchain(another.m_swapchain), m_imageCount(another.m_imageCount),
@@ -41,14 +42,15 @@ public:
   bool acquireNextImage(Fence const &signalFence,
                         uint64_t timeout = UINT64_MAX);
 
-  boost::container::small_vector<SwapChainImage, 3> retrieveImages();
-
+  auto &images() const { return m_images; }
   uint32_t currentImage() const;
   uint32_t imageCount() const { return m_imageCount; }
 
   virtual ~SwapChain();
 
-  Extension<ext::KHR_swapchain> const &extension() const { return m_swapchain_ext; }
+  Extension<ext::KHR_swapchain> const &extension() const {
+    return m_swapchain_ext;
+  }
 
   operator VkSwapchainKHR() const { return m_swapchain; }
 
@@ -59,6 +61,7 @@ private:
   std::optional<uint32_t> m_currentImage{};
   StrongReference<Device> m_device;
   Extension<ext::KHR_swapchain> m_swapchain_ext;
+  boost::container::small_vector<SwapChainImage, 3> m_images;
   VkSwapchainCreateInfoKHR m_createInfo;
   VkSwapchainKHR m_swapchain = VK_NULL_HANDLE;
 };
