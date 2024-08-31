6214af505ccd3a1ae31b2f883084f15dc8962a82
FixPlus <bushev.da@phystech.edu>
2024-08-31 15:54:25 +0300

    [feature] allow to create uninitialized StagingBuffer

--- include/vkw/StagingBuffer.hpp
+++ include/vkw/StagingBuffer.hpp
@@ -12,6 +12,7 @@ namespace vkw {
  */
 template <typename T> class StagingBuffer : public vkw::Buffer<T> {
 public:
+  // Create initialized.
   StagingBuffer(vkw::Device const &device, std::span<T const> data)
       : vkw::Buffer<T>(
             device, data.size(), VK_BUFFER_USAGE_TRANSFER_SRC_BIT,
@@ -20,6 +21,15 @@ public:
                                     .requiredFlags =
                                         VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT}) {
     std::copy(data.begin(), data.end(), vkw::Buffer<T>::mapped().begin());
+  }
+  // Create uninitialized.
+  StagingBuffer(vkw::Device const &device, size_t size): vkw::Buffer<T>(
+                                                 device, size, VK_BUFFER_USAGE_TRANSFER_SRC_BIT,
+                                                 VmaAllocationCreateInfo{.flags = VMA_ALLOCATION_CREATE_MAPPED_BIT,
+                                                                         .usage = VMA_MEMORY_USAGE_CPU_TO_GPU,
+                                                                         .requiredFlags =
+                                                                             VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT}){
+
   }
 };
 
